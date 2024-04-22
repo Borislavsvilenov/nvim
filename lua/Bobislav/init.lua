1468c018e93fce79366077b113e7e528f24393be
Bobislav <bobo.svilenov@gmail.com>
2024-04-22 20:00:29 +0300

    reverted to old config to fix no config outside config folder

--- lua/Bobislav/init.lua
+++ lua/Bobislav/init.lua
@@ -1,8 +1,18 @@
-require("Bobislav.remap")
-
-vim.cmd("set rnu")
+vim.g.mapleader = " "
 
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "a"
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 
--- lua/Bobislav/packer.lua
+++ lua/Bobislav/packer.lua
@@ -16,6 +16,7 @@ return require('packer').startup(function(use)
 	use { "rose-pine/neovim", as = "rose-pine" }
 	use { 'bluz71/vim-moonfly-colors', as = 'moonfly' }
 	use { 'bluz71/vim-nightfly-colors', as = 'nightfly' }
+	use { 'catppuccin/nvim', as = 'catppuccin' }
 
 	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 	use ('nvim-treesitter/playground')   
