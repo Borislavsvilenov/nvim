6a2f01fdca65e4f81ff201e318d06ce319eb7f12
Bobislav <bobo.svilenov@gmail.com>
2024-07-08 17:25:34 +0300

    c include path

--- after/plugin/lsp_config.lua
+++ after/plugin/lsp_config.lua
@@ -26,6 +26,9 @@ end
 require("lspconfig").pyright.setup({ on_attach = on_attach })
 require("lspconfig").tsserver.setup({ on_attach = on_attach })
 require("lspconfig").rust_analyzer.setup({ on_attach = on_attach })
-require("lspconfig").clangd.setup({ on_attach = on_attach, settings = { clangd = { path = "/usr/local/bin/clangd" } } })
+require("lspconfig").clangd.setup({ on_attach = on_attach, settings = { clangd = {
+  path = {"/usr/local/bin/clangd"},
+  includePath = {"/opt/homebrew/", "/usr/local/include/"}
+} } })
 require("lspconfig").cmake.setup({ on_attach = on_attach })
 require("lspconfig").lua_ls.setup({ on_attach = on_attach, settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
